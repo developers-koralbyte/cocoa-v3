f88c3cde020421f513d920aea03d24a2a24e76ab
Harsh <harshdugar1234@gmail.com>
2025-01-14 09:21:55 -0500

    Tailwind with typescript done

---
+++ my-project/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='font-bold text-2xl pt-6 py-10'>
+        Hello, World!
+      </div>
+    </>
+  )
+
+}
+
+export default App
