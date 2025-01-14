695151e7047d61f598e56daff7e94bde4e9f5a84
pyPrarthan <christianprarthan07@gmail.com>
2025-01-14 10:04:08 -0500

    Footer Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 
 import './App.css'
+import Footer from './assets/components/Footer'
 
 function App() {
   return (
     <>
-      <div className='font-bold text-2xl pt-6 py-10'>
-        Hello, World!
+      <div className='min-h-screen flex flex-col'>
+        <main className='flex-grow'>
+          <div className='min-h-screen bg-gray-100 items-center justify-center'>
+            <p>Start prompting (or editing) :) </p>
+          </div>
+        </main>
+      <Footer/>
       </div>
     </>
   )
