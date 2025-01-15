ad855011d8bb67ee2bad551153ad2459477d9de1
Harsh <harshdugar1234@gmail.com>
2025-01-15 12:21:43 -0500

    How it works sections and herosection

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import FAQ from "./components/FAQ";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
-
+import HowItWorks from "./components/HowItWorks";
 function App() {
   return (
 
     <>
       <nav><Navbar/></nav>
       <HeroSection/>
+      <HowItWorks/>
       <FAQ></FAQ>
       <footer><Footer/></footer>
       
