21dde12d28be2431f584e9062665964009e63edb
Bilal Arif <bilal.arif.siddiqui@gmail.com>
2025-01-21 08:45:07 +0500

    structure changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 
-import FAQ from "./components/Landing Page/FAQ";
-import Footer from "./components/Landing Page/Footer";
-import HowItWorks from "./components/Landing Page/HowItWorks";
-import LandingPage from "./components/Landing Page/LandingPage";
-import ColaborationSection from "./components/Landing Page/CollaborationSection";
-import LatestNews from "./components/Landing Page/LatestNews";
-import UserTestimonial from "./components/Landing Page/UserTestimonial";
+import FAQ from "./components/HomePage/FAQ";
+import Footer from "./components/HomePage/Footer";
+import HowItWorks from "./components/HomePage/HowItWorks";
+import LandingPage from "./components/HomePage/LandingPage";
+import ColaborationSection from "./components/HomePage/CollaborationSection";
+import LatestNews from "./components/HomePage/LatestNews";
+import UserTestimonial from "./components/HomePage/UserTestimonial";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
       <UserTestimonial/>
       <LatestNews/>
       <HowItWorks/>
-      <FAQ></FAQ>
+      <FAQ/>
       <footer><Footer/></footer>
       
     </>
