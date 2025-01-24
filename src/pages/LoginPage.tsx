dedd204fa79e2dfd3c67b7fb5caca1cb5d40dc8d
Harsh <harshdugar1234@gmail.com>
2025-01-24 16:17:40 -0500

    Login page and Forgot password page done

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,42 @@
-
-import React from 'react';
+import React from "react";
+import image1 from "../assets/img/login/LoginImage.png";
+import LoginForm from "../components/LoginPage/LoginForm";
+import logo from "../assets/img/login/cocoaLoginLogo.png";
 
 const LoginPage: React.FC = () => {
   return (
-    <div className="flex flex-center justify-center">
-        
+    <div className="min-h-full flex">
+      {/* Left Section with the Image */}
+      <div className="lg:w-1/2 w-full h-[1050px]">
+        <img
+          src={image1}
+          alt="Welcome Back Illustration"
+          className="w-full h-full object-cover"
+        />
+      </div>
 
+      {/* Right Section */}
+      <div className="lg:w-1/2 w-full h-screen flex items-center justify-center">
+        <div className="max-w-md w-full px-8 text-center">
+          {/* Logo */}
+          <img
+            src={logo}
+            alt="Cocoa Logo"
+            className="mx-auto mb-3 h-auto w-auto"
+          />
+
+          {/* Welcome Text */}
+          <div className="font-sourceSans text-[25px] text-gray-700 mb-10">
+            <p className="mb-2">Welcome Back!</p>
+            <p>Please log in to your account.</p>
+          </div>
+
+          {/* Login Form */}
+          <LoginForm />
+        </div>
+      </div>
     </div>
   );
 };
-    
 
 export default LoginPage;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
-
+import ForgotPasswordPage from  "../pages/ForgotPasswordPage";
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
-      
+      <Route path="forgot-password" element={<ForgotPasswordPage/>}/>
     </Routes>
   );
 };
