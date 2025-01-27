871f495af6fbc8f7390f0948f3143049a843000b
pyPrarthan <christianprarthan07@gmail.com>
2025-01-27 17:37:01 -0500

    User & Buyer section

---
+++ src/pages/VerificationBuyer.tsx
@@ -0,0 +1,43 @@
+import checkMark from "../assets/img/Verification/checkmark.png";
+import cocoaImg from "../assets/img/cocoa-logo.png";
+
+const VerificationSuccess = () => {
+  return (
+    <div className="min-h-screen bg-white flex flex-col items-center justify-center p-4 relative">
+      {/* Cocoa logo in the top-left corner */}
+      <div className="absolute top-4 left-4 mb-6">
+       <a href="https://cocoa-app.com/" target="_blank" rel="noopener noreferrer">
+          <img src={cocoaImg} alt="Cocoa Logo" className="w-[140px] h-[100px]" />
+        </a>
+      </div>
+
+      {/* Main content */}
+      <div className="mb-8">
+        <img
+          src={checkMark}
+          alt="Check Mark"
+          className="max-w-[150px] h-auto mb-6"
+        />
+      </div>
+
+      <h1 className="text-3xl font-bold text-gray-900 mb-4 text-center font-nunito">
+        Your Account is Now Verified!
+      </h1>
+
+      <p className="text-gray-600 text-center max-w-md mb-12 font-nunito">
+        Congratulations, your email address has been successfully verified!
+        You're all set to explore and enjoy everything{" "}
+        <span className="font-semibold">COCOA</span> has to offer.
+      </p>
+
+      <button
+        className="px-8 py-3 bg-[#7C77C1] text-white rounded-full hover:bg-[#6661B0] transition-colors"
+        onClick={() => (window.location.href = "/dashboard")}
+      >
+        Start Exploring
+      </button>
+    </div>
+  );
+};
+
+export default VerificationSuccess;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
+import NewBuyer from "../pages/NewBuyer";
+import NewVendor from "../pages/NewVendor";
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
-      
+      <Route path='/newBuyer' element={<NewBuyer/>} />
+      <Route path='/newVendor' element={<NewVendor/>} />
     </Routes>
   );
 };
