2894f2df4352583461150d07cce7e3c153c18a92
Harsh <harshdugar1234@gmail.com>
2025-01-29 13:16:30 -0500

    Verification and new password page

--- src/pages/Forgot_Password/ForgotPasswordPage.tsx
+++ src/pages/Forgot_Password/ForgotPasswordPage.tsx
@@ -6,18 +6,21 @@ import logo from "../../assets/img/login/cocoaLoginLogo.png";
 
 const ForgotPasswordPage: React.FC = () => {
   return (
-    <div className="min-h-full flex ">
+    <div className="min-h-full flex">
       {/* Left Section with the Image */}
-      <div className="lg:w-1/2 w-full h-[1050px]">
-        <img
-          src={background}
-          alt="Welcome Back Illustration"
-          className="w-full h-full object-cover"
-        />
+      <div className="lg:w-1/2 w-full h-screen bg-purple-100"> {/* Add background color */}
+        <div className="w-full h-full relative"> {/* Add wrapper div */}
+          <img
+            src={background}
+            alt="Welcome Back Illustration"
+            className="w-full h-full object-cover absolute inset-0"
+            loading="eager" 
+          />
+        </div>
       </div>
 
       {/* Right Section */}
-      <div className="lg:w-1/2 h-[1000px] flex justify-center ">
+      <div className="lg:w-1/2 h-[898px] flex justify-center ">
         <div className="mt-28 max-w-md w-full px-8 text-center  ">
           {/* Logo */}
           <img
