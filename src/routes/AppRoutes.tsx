5437a381337bfce217493a3ea733a6b3b87aa59f
pyPrarthan <christianprarthan07@gmail.com>
2025-02-06 02:35:43 -0500

    Buyer & Vendor Dashboard

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,35 +1,41 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import HomePage from '../pages/HomePage'
-import LoginPage from '../pages/LoginPage'
-import NewBuyer from '../pages/NewBuyer'
-import NewVendor from '../pages/NewVendor'
-import ForgotPasswordPage from '../pages/Forgot_Password/ForgotPasswordPage'
-import NewPasswordPage from '../pages/Forgot_Password/NewPasswordPage'
-import VerificationPage from '../pages/Forgot_Password/VerificationPage'
-import SignupSelectionPage from '../pages/SignUpSelectionPage'
-import Dashboard from '../pages/Dashboard/Dashboard'
-import InvoicePage from '../pages/Dashboard/InvoicesPage'
-import CalendarPage from '../pages/Dashboard/CalendarPage'
-import InboxPage from '../pages/Dashboard/InboxPage'
+import React, { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// Lazy load all the components
+const HomePage = React.lazy(() => import('../pages/HomePage'));
+const LoginPage = React.lazy(() => import('../pages/LoginPage'));
+const NewBuyer = React.lazy(() => import('../pages/NewBuyer'));
+const NewVendor = React.lazy(() => import('../pages/NewVendor'));
+const ForgotPasswordPage = React.lazy(() => import('../pages/Forgot_Password/ForgotPasswordPage'));
+const NewPasswordPage = React.lazy(() => import('../pages/Forgot_Password/NewPasswordPage'));
+const VerificationPage = React.lazy(() => import('../pages/Forgot_Password/VerificationPage'));
+const SignupSelectionPage = React.lazy(() => import('../pages/SignUpSelectionPage'));
+const VendorDashboard = React.lazy(() => import('../pages/Dashboard/VendorDashboard'));
+const BuyerDashboard = React.lazy(() => import('../pages/Dashboard/BuyerDashboard'));
+const InvoicePage = React.lazy(() => import('../pages/Dashboard/InvoicesPage'));
+const CalendarPage = React.lazy(() => import('../pages/Dashboard/CalendarPage'));
+const InboxPage = React.lazy(() => import('../pages/Dashboard/InboxPage'));
 
 const AppRoutes: React.FC = () => {
     return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/create-account" element={<SignupSelectionPage />} />
-            <Route path="/new-buyer" element={<NewBuyer />} />
-            <Route path="/new-vendor" element={<NewVendor />} />
-            <Route path="forgot-password" element={<ForgotPasswordPage />} />
-            <Route path="/new-password" element={<NewPasswordPage />} />
-            <Route path="/verification" element={<VerificationPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/invoices" element={<InvoicePage />} />
-            <Route path="/calendar" element={<CalendarPage />} />
-            <Route path="/inbox" element={<InboxPage />} />
-        </Routes>
-    )
-}
+        <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/create-account" element={<SignupSelectionPage />} />
+                <Route path="/new-buyer" element={<NewBuyer />} />
+                <Route path="/new-vendor" element={<NewVendor />} />
+                <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+                <Route path="/new-password" element={<NewPasswordPage />} />
+                <Route path="/verification" element={<VerificationPage />} />
+                <Route path="/vendor-dashboard" element={<VendorDashboard />} />
+                <Route path="/buyer-dashboard" element={<BuyerDashboard />} />
+                <Route path="/invoices" element={<InvoicePage />} />
+                <Route path="/calendar" element={<CalendarPage />} />
+                <Route path="/inbox" element={<InboxPage />} />
+            </Routes>
+        </Suspense>
+    );
+};
 
-export default AppRoutes
+export default AppRoutes;
